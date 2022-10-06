e8d3316222b173165d440e00313ba4b959d00826
rocco1967 <gianfranco.fa@gmail.com>
2022-10-06 21:22:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from PIL import Image
 from lineartree import LinearTreeRegressor,LinearBoostRegressor
 image=Image.open('sfera.JPG')
 st.image(image)
-st.header('...................MERLIN.....................')
+st.subheader('_____________________________MERLIN____________________________')
 st.title('...............STOCK_FORECAST.............')
 st.markdown('(CL=F)=CRUDE_OIL', unsafe_allow_html=False)
 st.markdown('AAPL= APPLE', unsafe_allow_html=False)
