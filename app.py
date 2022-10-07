891359f64d0dd95abb065bf1980e652ceb48f87e
rocco1967 <gianfranco.fa@gmail.com>
2022-10-07 11:47:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,11 @@ def new_data():
     data1['Date'] = pd.to_datetime(data1['Date'],format='%Y%m%d').dt.date
     data1=data1.set_index('Date')
     data1.fillna(data1.mean(),inplace=True)
-        #data1['CL=F'] = data1['CL=F'].fillna(data1['CL=F'].rolling(2).mean())
+    if tickers=='CL=F':
+        data1=data1[:-1]
+    else:
+        data1=data1
+       
     return data1#[:-1]
 model = pickle.load(open('stocks.pk','rb'))
 x1=(new_data()[-7:].values.flatten()).reshape(1,-1)
