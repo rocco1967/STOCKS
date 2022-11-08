e8c2e033166cf161e5418ab3d5fb2929487d8c96
rocco1967 <gianfranco.fa@gmail.com>
2022-11-08 15:57:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ st.markdown(
 )
 new_data3=(new_data[-14:].values.flatten()).reshape(-1,7)
 yhat2=model.predict(new_data3)
-st.write(yhat2)
-st.subheader(new_data3)
+#st.write(yhat2)
+#st.subheader(new_data3)
 if st.button('FORECAST'):
    prediction=yhat#np.where(yhat>'filter',yhat,0)
    st.subheader(f' FORECAST + un giorno in archivio ... +- 2% ..   {prediction[0]:.2f} USD')
