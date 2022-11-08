2378533226b0072901dab3d46e122094ceac018d
rocco1967 <gianfranco.fa@gmail.com>
2022-11-08 15:25:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,9 @@ st.markdown(
     unsafe_allow_html=True,
 )
 new_data3=(new_data.values.flatten()).round(2)
-st.subheader(new_data3)
+yhat2=model.predict(new_data3)
+st.write(yhat2)
+#st.subheader(new_data3)
 if st.button('FORECAST'):
    prediction=yhat#np.where(yhat>'filter',yhat,0)
    st.subheader(f' FORECAST + un giorno in archivio ... +- 2% ..   {prediction[0]:.2f} USD')
