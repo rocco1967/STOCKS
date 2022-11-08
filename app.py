814943f40a751f28ad3fb5aa0453034f0cfb2944
rocco1967 <gianfranco.fa@gmail.com>
2022-11-08 15:32:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,9 +90,9 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-new_data3=(new_data[-14:].values.flatten()).reshape(1,-1)
+new_data3=(new_data.values.flatten()).reshape(1,-1)
 yhat2=model.predict(new_data3)
-st.write(yhat2)
+#st.write(yhat2)
 #st.subheader(new_data3)
 if st.button('FORECAST'):
    prediction=yhat#np.where(yhat>'filter',yhat,0)
