d028f6a70a1ee81143ec6ea7b351a360a0920002
rocco1967 <gianfranco.fa@gmail.com>
2022-11-08 16:27:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ if new_data()[-1:].index.values==np.array(now):
 else:
     new_data=new_data() 
 model = pickle.load(open('stocks.pk','rb'))
-x1=(new_data[-7:].values.flatten()).reshape(1,-1)
+x1=(new_data[-7:].values.flatten()).reshape(-1,-7)
 yhat=model.predict(x1).round(2)
 #st.title('........STOCK_FORECAST.........')
 #st.sidebar.subheader('ULTIMO DATO IN ARCHIVIO DEL SIMBOLO SCELTO PER IL CALCOLO')
