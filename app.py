e39e09e1f0041f3902eb50391e17858cd3f43212
rocco1967 <gianfranco.fa@gmail.com>
2022-12-22 10:08:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ st.write(((x_new_data3.flatten().reshape(-1,7)))[-7:])
 
 #new_data3=(new_data[-21:].values.flatten()).reshape(-1,7)
 yhat2=model.predict(x_new_data3.flatten().reshape(-1,7))
-st.write(x_new_data3[-2:])
+st.write(x_new_data3[-1:])
 dfyhat = pd.DataFrame(data=yhat2 )
 #change=abs(dfyhat.pct_change().dropna())
 #yhat2=np.where(change>0.015,'TRADE','STAY_FLAT')###  ORIGINALE
