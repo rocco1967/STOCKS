4af56c298b25a26f024ea20bfb330438b161132d
rocco1967 <gianfranco.fa@gmail.com>
2022-12-27 12:37:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ df_roll_filtered['system']=(((10000*df_roll_filtered['equity_com_sl']).cumsum()+
 chart_data = pd.DataFrame(
     df_roll_filtered['system'].values,
     columns=['merlin_system'])
-st.bar_chart(chart_data)
+st.plotly_chart(chart_data)
