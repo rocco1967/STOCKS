bbe7affc15113a7f096396a7aaa7bb66a12c6b24
rocco1967 <gianfranco.fa@gmail.com>
2022-12-27 13:18:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -192,8 +192,10 @@ import seaborn as sns
 #equity=((1000*df_roll['equity_sl']).cumsum()+10000)
 df_roll_filtered['system']=(((10000*df_roll_filtered['equity_com_sl']).cumsum()+10000)+254)-10000#,color='red',label='MERLIN_SYSTEM')
 df_roll_filtered['real']=((df_roll['real_roll_%change']*10000).cumsum()+10000+150)-10000#,color='black',label='REALE')
-chart_data = pd.DataFrame(
-    [df_roll_filtered['system'].values],[df_roll_filtered['real'].values],
-    columns=['merlin_system','real'])
+data=[[df_roll_filtered['system'].values],[df_roll_filtered['real'].values]]
+#chart_data = pd.DataFrame(
+    #[df_roll_filtered['system'].values],[df_roll_filtered['real'].values],
+    #columns=['merlin_system','real'])
+chart_data = pd.Data_frame(data,columns=['merlin_system','real'])    
 st.line_chart(chart_data)
 st.write(server_time)
