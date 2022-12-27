9a635b93d618f08d84d860f6f471aec78cc40906
rocco1967 <gianfranco.fa@gmail.com>
2022-12-27 12:13:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ system=(((10000*df_roll_filtered['equity_com_sl']).cumsum()+10000)+280)#,color='
 #real=((df_roll['real_roll_%change']*10000).cumsum()+10000+150)#,color='black',label='REALE')
 #plt.legend(loc="upper center")
 #system=np.array(system)
-st.write(system)
+st.write(system['equity_com_sl'])
 #fig, ax = plt.subplots()#figsize=(7, 3))
 #fig = plt.figure(figsize=(10, 4))
 #sns.lineplot(x = "distance", y = "mass", data = data_frame)
