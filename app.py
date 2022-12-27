3219febc131495f2f47c3149704c06a0caaeeb5a
rocco1967 <gianfranco.fa@gmail.com>
2022-12-27 12:16:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -190,11 +190,11 @@ from matplotlib.pyplot import figure
 #plt.plot((df_roll['pred_roll_%change']).rolling(7).std())
 import seaborn as sns
 #equity=((1000*df_roll['equity_sl']).cumsum()+10000)
-system=(((10000*df_roll_filtered['equity_com_sl']).cumsum()+10000)+280)#,color='red',label='MERLIN_SYSTEM')
+df_roll_filtered['system']=(((10000*df_roll_filtered['equity_com_sl']).cumsum()+10000)+280)#,color='red',label='MERLIN_SYSTEM')
 #real=((df_roll['real_roll_%change']*10000).cumsum()+10000+150)#,color='black',label='REALE')
 #plt.legend(loc="upper center")
 #system=np.array(system)
-st.write(system['equity_com_sl'])
+st.write(df_roll_filtered['equity_com_sl'])
 #fig, ax = plt.subplots()#figsize=(7, 3))
 #fig = plt.figure(figsize=(10, 4))
 #sns.lineplot(x = "distance", y = "mass", data = data_frame)
