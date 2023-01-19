a703a186cf1a2587714ba33412a43e7247e7f800
rocco1967 <gianfranco.fa@gmail.com>
2023-01-19 10:17:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,8 @@ y = np.array(y)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle=False)
 model = pickle.load(open('stocks_RF.pk','rb'))
 pred=model.predict(x_test)
-df_roll=pd.DataFrame(y_test.reshape(-1,1)[-41:],columns=['real_roll'])#,pred.reshape(-1,1)[-10:-1]
-df_roll['pred_roll']=pd.DataFrame(pred.reshape(-1,1)[-41:],columns=['pred_roll'])
+df_roll=pd.DataFrame(y_test.reshape(-1,1)[-39:],columns=['real_roll'])#,pred.reshape(-1,1)[-10:-1]
+df_roll['pred_roll']=pd.DataFrame(pred.reshape(-1,1)[-39:],columns=['pred_roll'])
 df_roll['pred_change']=np.where(df_roll['pred_roll'].pct_change()>0,1,-1)
 df_roll['real_change']=np.where(df_roll['real_roll'].pct_change()>0,1,-1)
 df_roll['real_roll_%change']=(df_roll['real_roll'].pct_change())
