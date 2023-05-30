09ca36c615909965f0d94f4b341dfa1808ce0374
rocco1967 <gianfranco.fa@gmail.com>
2023-05-30 15:19:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Thu Oct  6 13:05:02 2022
 """
 import streamlit as st
 st.set_page_config(page_title="AI side hustle")
-st.subheader(' Artificial IntelligenceTrading Signal Generator on WTI')
+st.subheader(' Artificial Intelligence_Trading Signal Generator on WTI')
 import numpy as np
 import pandas as pd
 import pickle
